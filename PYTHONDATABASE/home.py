145e83d21ee9ebd7d97bd2e7f610aef21c1d497a
unknown <you@example.com>
2025-08-23 04:31:49 -0700

    second commit

---
+++ PYTHONDATABASE/home.py
@@ -0,0 +1,12 @@
+print("welcome home")
+
+def drive():
+    print("I just learn how to drive")
+
+    # new addition
+    def details():
+        name = "Ephraim tobi"
+        address = "Ijaiye"
+        peint(name + address)
+
+        details()
