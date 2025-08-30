45dea3a16bcb550951542f475e29e540e2931151
unknown <you@example.com>
2025-08-30 05:20:54 -0700

    first commit

---
+++ PYTHONDATABASE/new.py
@@ -0,0 +1,45 @@
+a = 200
+b = 3300
+
+if(b > a):
+    print("b is greater than a")
+elif b == a :
+    print(" this are equal value")
+else :
+    print("a is greater than b")
+
+    # loops
+    #for loops , while loops
+
+    for x in range(10):
+        print(x)
+
+fruits = ["mango","cherry","banana"]
+
+print(fruits) 
+
+
+# print(fruits)
+
+# for x in fruits :
+# print(x)
+
+for y in "dammy":
+    print(y)
+
+# while loop 
+
+i = 1
+while i < 6 :
+    print(i)
+    i +=1
+
+i = 20
+while i < 100 :
+    print(i)
+    i +=20
+
+
+def play():
+    print("i love playing")
+play()
